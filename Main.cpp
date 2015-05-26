11386612f855f1ab99fde97df703785295c396f4
Syriamanal <shadinachat123@gmail.com>
2015-05-26 08:47:08 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,7 @@ public:
     void Maps(){
         char* currentMap;
         char Map[10][10] = {
-                {"#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"},
+                {"#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#",},
                 {"#", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#"},
                 {"#", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#"},
                 {"#", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#"},
@@ -23,14 +23,23 @@ public:
         int playerCol = 2;
 
         while(true){
-            if(GetAsyncKeyState(VK_UP))
+            if(GetAsyncKeyState(VK_UP)) {
                 playerRow -= 1;
-            if(GetAsyncKeyState(VK_DOWN))
+                clearscreen();
+            }
+            if(GetAsyncKeyState(VK_DOWN)) {
                 playerRow += 1;
-            if(GetAsyncKeyState(VK_RIGHT))
+                clearscreen();
+            }
+            if(GetAsyncKeyState(VK_RIGHT)) {
                 playerCol -= 1;
-            if(GetAsyncKeyState(VK_LEFT))
+                clearscreen();
+            }
+            if(GetAsyncKeyState(VK_LEFT)) {
                 playerCol += 1;
+                clearscreen();
+            }
+            
 
             for(row = 0; row < 10; ++row)
             {
@@ -41,19 +50,19 @@ public:
                     else
                         cout << currentMap[row][col];
                     if(currentMap[playerRow][playerCol] == "#") {
-                        system("CLS");
+                        clearscreen();
                         cout << "Game Over!";
                     }
-                        else if (currentMap[playerRow][playerCol] == "|"){
-                            int playerRow = 2;
-                            int playerCol = 2;
-                        }
+                    else if (currentMap[playerRow][playerCol] == "|"){
+                        int playerRow = 2;
+                        int playerCol = 2;
                     }
-                    cout << endl;
                 }
+                cout << endl;
             }
-                system("pause");
         }
+        system("pause>nul");
+    }
 };
 
 int main() {
@@ -66,9 +75,9 @@ int main() {
 void clearscreen(){
     HANDLE hOut;
     COORD Position;
-    
+
     hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    
+
     Position.X = 0;
     Position.Y = 0;
     SetConsoleCursorPosition(hOut, Position);
